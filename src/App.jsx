960beeed470467376cc78d77c7d0760e2833e3ee
Aleks Kowalkowski <alekskowalkowski@gmail.com>
2023-03-23 19:50:01 -0400

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, ForgotPassword, SignIn, SignUp, Offers, Profile } from "./pages";
+
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} /> 
+                    <Route path="/profile" element={<Profile />} /> 
+                    <Route path="/sign-in" element={<SignIn />} /> 
+                    <Route path="/sign-up" element={<SignUp />} /> 
+                    <Route path="/forgot-password" element={<ForgotPassword />} /> 
+                    <Route path="/offers" element={<Offers />} /> 
+                </Routes>
+            </Router>
+        </>
     );
 };
 
